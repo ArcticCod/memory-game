2fd08146b3a5033ac903e2f727224dbc1a4eb68f
ArcticCod <Kokkoalex18@gmail.com>
2023-11-27 19:24:44 -0800

    removed nest

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -9,10 +9,7 @@ const Modal = ({ setIsOpen, score, highScore, setScore }) => {
           <div className={"modalHeader"}>
             <h5 className={"heading"}>Game Over!</h5>
           </div>
-          <button
-            className={"closeBtn"}
-            onClick={() => setIsOpen(false)}
-          ></button>
+
           <div className={"modalContent"}>
             Score: {score} - Best: {highScore}
           </div>
